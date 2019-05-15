13a79654030cde9c5453918632f1c7c05bc545ba
George Shpak <georgeshpak@Georges-MacBook-Air.local>
2019-05-14 22:51:33 -0700

    Intial Commit. Need to add javascript

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+let friends = [
+    {
+        name: "LeBron James",
+        photo: "https://www.google.com/search?q=lebron+james&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiW4tbWopriAhXC3J4KHZYUCb0Q_AUIDygC&biw=1035&bih=722#imgrc=2FK-eebBTQjM4M: ",
+        scores: [1,2,3,4,5,4,3,2,1,2],
+    },
+    {
+        name: "LeBron James 2",
+        photo: "https://www.google.com/search?q=lebron+james&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiW4tbWopriAhXC3J4KHZYUCb0Q_AUIDygC&biw=1035&bih=722#imgrc=2FK-eebBTQjM4M: ",
+        scores: [5,4,3,2,1,2,3,4,1,2],
+    },
+    {
+        name: "LeBron James 3",
+        photo: "https://www.google.com/search?q=lebron+james&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiW4tbWopriAhXC3J4KHZYUCb0Q_AUIDygC&biw=1035&bih=722#imgrc=2FK-eebBTQjM4M: ",
+        scores: [3,3,3,3,3,3,3,3,3,3],
+    },
+    {
+        name: "LeBron James 4",
+        photo: "https://www.google.com/search?q=lebron+james&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiW4tbWopriAhXC3J4KHZYUCb0Q_AUIDygC&biw=1035&bih=722#imgrc=2FK-eebBTQjM4M: ",
+        scores: [2,2,2,2,2,2,2,2,2,2],
+    },
+    {
+        name: "LeBron James 5",
+        photo: "https://www.google.com/search?q=lebron+james&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiW4tbWopriAhXC3J4KHZYUCb0Q_AUIDygC&biw=1035&bih=722#imgrc=2FK-eebBTQjM4M: ",
+        scores: [1,1,1,1,1,1,1,1,1,1],
+    },
+
+
+];
+
+module.exports = friends;
